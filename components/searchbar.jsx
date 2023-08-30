b75abd413d5fae3df2b4ffdb8e68dbd983c2beb9
ramazan <ramazancinar0858@gmail.com>
2023-08-30 21:18:56 +0200

    searchBar component : adding state & onChange value

--- components/searchbar.jsx
+++ components/searchbar.jsx
@@ -1,8 +1,18 @@
-export default function SearchBsr() {
+import { useState } from "react"
+
+export default function SearchBar() {
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+    }
+
     return (
         <input
             type="text"
             placeholder="Search a pokemon"
+            value={searchValue}
+            onChange={handleSearchChange}
         />
     )
 }
