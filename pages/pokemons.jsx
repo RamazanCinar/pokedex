da1c439b66e9b93c2746891b1f33f45213451c55
ramazan <ramazancinar0858@gmail.com>
2023-09-16 09:56:03 +0200

    Pokemons page : adding try/catch bloc

--- pages/pokemons.jsx
+++ pages/pokemons.jsx
@@ -7,10 +7,24 @@ export default function Pokemons() {
     const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
-        .then((response) => response.json())
-        .then((data) => setPokemons(data.results));
-    }, []);
+        try {
+          fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error('Network error');
+              }
+              return response.json();
+            })
+            .then((data) => setPokemons(data.results))
+            .catch((error) => {
+              console.error('Error fetching data:', error);
+            });
+        } catch (error) {
+
+          console.error('Error:', error);
+
+        }
+      }, []);
 
     return (
         <div>
