e8c9f46bfb377d5161a281389f179adf871f7ab9
ramazan <ramazancinar0858@gmail.com>
2023-09-16 15:22:09 +0200

    About page : adding few styles

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,12 +2,14 @@ import Header from "@/components/header";
 
 export default function About() {
     return (
-        <div>
+        <>
             <Header />
-            <h1>About</h1>
-            <p>Hi! I'm a web developer, and I'd like to introduce you to my project: the Pokédex.
-                I chose Pokémon as the theme to enhance my development skills, simply because Pokémon aare cool! 
-                Explore and discover my work. </p>
-        </div>
+            <div className="">
+                <div className="text-center">About</div>
+                <div className=" w-4/6 flex mx-auto">Hi! I'm a web developer, and I'd like to introduce you to my project: the Pokédex.
+                    I chose Pokémon as the theme to enhance my development skills, simply because Pokémon aare cool! 
+                    Explore and discover my work. </div>
+            </div>
+        </>
     )
 }
