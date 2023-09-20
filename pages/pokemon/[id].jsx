06fb608e75ee18175d5a02cc47cd62271f04e68e
ramazan <ramazancinar0858@gmail.com>
2023-09-20 15:30:42 +0200

    pokemon detail: adding pokemon data & image

--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -1,17 +1,52 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const PokemonDetails = ({ pokemon }) => {
+import Image from 'next/image';
+
+const PokemonDetails = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [pokemonData, setPokemonData] = useState(null);
 
-  if (!id) {
+useEffect(() => {
+    if (!id) return;
+
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+    fetch(apiUrl)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Error network');
+        }
+        return response.json();
+    })
+    .then((data) => {
+        setPokemonData(data);
+    })
+    .catch((error) => {
+        console.error('Error fetching data');
+    });
+}, [id])
+
+if (!id) {
     return <div>Error. Please try again later.</div>;
   }
 
+  if (!pokemonData) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <div>
-      <h1>Pokémon Details: {id}</h1>
-      <h1>{pokemon.name}</h1>
+      <h1>Pokémon No: {id}</h1>
+      <h2>Name: {pokemonData.name}</h2>
+      <Image 
+            src={pokemonData.sprites.other.dream_world.front_default} 
+            width={100}
+            height={100}
+            alt={`image of ${pokemonData.name}`}
+            />  
     </div>
   );
 };
