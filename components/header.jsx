b68d3c6db88a4869966dcb87df9718f94a806d0e
Ramazan <ramazancinar0858@gmail.com>
2023-09-21 10:41:42 +0200

    adding font

--- components/header.jsx
+++ components/header.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Header() {
     return (
         <div>
-            <h1 className="flex justify-center text-3xl text-red-500">Pokédex</h1>
+            <h1 className="flex justify-center text-5xl text-red-500 ">Pokédex</h1>
             <ul className=" my-2 flex justify-center flex-wrap space-x-3">
                 <li>
                     <Link href="/">Home</Link>
--- pages/layout.jsx
+++ pages/layout.jsx
@@ -1,9 +1,13 @@
-import { Inter } from 'next/font/google';
+import { Tektur } from 'next/font/google';
 
 import './globals.css'
 
 
-const inter = Inter({ subsets: ['latin'] })
+const tektur = Tektur({ 
+  weight: '400',
+  subsets: ['latin'], 
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Pokédex',
@@ -12,8 +16,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr" className={tektur.className}>
+      <body>{children}</body>
     </html>
   )
 }
